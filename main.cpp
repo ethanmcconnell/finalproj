118a53d36e65737f341c6fe663105761260b15c3
ethanmcconnell <edm22@fsu.edu>
2023-12-05 00:07:27 -0500

    changed the nums into a string. it now run well, except if a decimal is entered into num1 or num2. need to create an exception for it. also, could be organized

--- main.cpp
+++ main.cpp
@@ -6,20 +6,35 @@ using namespace std;
 int main () {
 
     bool anotherOperation = true;
+
     vector <double> firstNum;
     vector <string> theOperator;
     vector <double> secondNum;
     vector<double> previousVals;
 
     while (anotherOperation) {
-        double num1, num2, result;
+        string num1, num2;
+        double result;
         string oper;
         string oneMore;
+        bool numberOneBool = true;
+        bool numberTwoBool = true;
 
+        while (numberOneBool) {
+            cout << "Enter first number:" << endl;
+            cin >> num1;
+            numberOneBool = false;
 
-        cout << "Enter first number:" << endl;
-        cin >> num1;
-        firstNum.push_back(num1);
+            for (int i = 0; i < num1.size(); ++i) {
+                if (!isdigit(num1[i])) {
+                    numberOneBool = true;
+                    cout << "Please enter a valid number." << endl;
+                    break;
+                }
+            }
+        }
+        double numOneDouble = std::stod(num1);
+        firstNum.push_back(numOneDouble);
 
         while (true) {
             cout << "Enter operator: +, -, *, /" << endl;
@@ -32,11 +47,23 @@ int main () {
         }
         theOperator.push_back(oper);
 
+        while (numberTwoBool) {
         cout << "Enter second number" << endl;
         cin >> num2;
-        secondNum.push_back(num2);
+        numberTwoBool = false;
 
-        result = NumsResult(num1, oper, num2);
+            for (int i = 0; i < num2.size(); ++i) {
+                if (!isdigit(num2[i])) {
+                    numberTwoBool = true;
+                    cout << "Please enter a valid number." << endl;
+                    break;
+                }
+            }
+        }
+        double numTwoDouble = std::stod(num2);
+        secondNum.push_back(numTwoDouble);
+
+        result = NumsResult(numOneDouble, oper, numTwoDouble);
 
         if (oper == "+") {
             cout << "The sum is: " << result << endl;
